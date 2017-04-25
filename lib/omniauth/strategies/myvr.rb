f918c21d8c593a76640d5216f177a61dc80a81b4
Sana <syedsanahassan@gmail.com>
2017-04-24 21:28:14 -0500

    Fix the CSRF token issue.

--- lib/omniauth/strategies/myvr.rb
+++ lib/omniauth/strategies/myvr.rb
@@ -37,6 +37,12 @@ module OmniAuth
         verifier = request.params['code']
         client.auth_code.get_token(verifier, token_params)
       end
+      
+      # Fixes regression in omniauth-oauth2 v1.4.0 by
+      # https://github.com/intridea/omniauth-oauth2/commit/85fdbe117c2a4400d001a6368cc359d88f40abc7
+      def callback_url
+        options[:redirect_uri] || (full_host + script_name + callback_path)
+      end
     end
   end
 end
